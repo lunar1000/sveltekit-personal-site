5de5281392c59ea2b7eb360135d52779e6ed1165
unknown <you@example.com>
2023-10-22 14:24:17 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let data;
+</script>
+
+<div class="row">
+	<div class="col">
+		<h1>Awesome Our Website</h1>
+		<p>우리 마을</p>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<h2>Daily Quote</h2>
+		<p>{data.quote}</p>
+		<p>{data.author}</p>
+	</div>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<div class="row">
+	<div class="col">
+		<h1>About</h1>
+	</div>
+</div>
+<div class="row">
+	<div class="col">
+		<p>멋진 사이트</p>
+	</div>
+</div>
